6fcf6fad503d385c9b043860c234f02887335e51
Diego Correa Tristain <algoritmia@labormedia.cl>
2024-11-01 07:08:40 +0000

    Refactors address_lookup. Adds update_address_nonce to model::db

--- src/model/db.rs
+++ src/model/db.rs
@@ -11,22 +11,19 @@ pub async fn address_lookup(client: web::Data<Client>, credentials: Credentials<
                     let address_xpub: bip32::Xpub = address.xpub.clone().to_xpub();
                     let mut address_message = address_xpub.to_string().to_owned();
                     address_message.push_str(&address.nonce.clone().to_str());
-                    println!("to sign B {}", address_message);
                     let message_hash = signed_msg_hash(&address_message);
-                    // Update nonce on persistent db
-                    let updated_address = address.clone();// .update_nonce(); // TODO: Unleash the nonce updating procedure.
-                    match collection.insert_one(updated_address.clone()).await {
-                        Ok(_) => { 
-                            if credential_xpub.encode() != address.xpub.to_bytes() {
-                                Err(HttpResponse::Unauthorized().json("Unauthorized"))
-                            } else {
-                                Ok(updated_address)
-                            }
-                         },
-                        Err(err) => Err(HttpResponse::InternalServerError().body(err.to_string())),
-                    }
+                    Ok(address)
                 },
                 Ok(None) => Err(HttpResponse::NotFound().json("NotFound")),
                 Err(err) => Err(HttpResponse::InternalServerError().body(err.to_string())),
             }
+}
+
+pub async fn update_address_nonce(collection: Collection<Address<XpubWrapper>>, address: Address<XpubWrapper>) -> Result<Address<XpubWrapper>, HttpResponse>  {
+    // Update nonce on persistent db
+    let updated_address = address.clone().update_nonce(); // TODO: Unleash the nonce updating procedure.
+    match collection.insert_one(updated_address.clone()).await {
+        Ok(_) => Ok(updated_address),
+        Err(err) => Err(HttpResponse::InternalServerError().body(err.to_string())),
+    }
 }
